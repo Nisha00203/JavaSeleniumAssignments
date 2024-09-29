def00132c69e7100006dfd9762c4c392165e272f
Nisha00203 <138268793+Nisha00203@users.noreply.github.com>
2024-09-30 00:11:57 +0530

    Add files via upload

---
+++ Reverse_String.java
@@ -0,0 +1,30 @@
+package String_Demo;
+
+public class Reverse_String {
+
+public static void main(String[] args)
+	
+	{
+				
+		String s1= "madam";
+		String output ="";
+		for(int i=s1.length()-1;i>=0;i--)
+		{			
+			char c1=s1.charAt(i);
+			output=output+c1;
+			
+		}		
+	    System.out.println(output);
+	    if(s1.equalsIgnoreCase(output)==true)
+    	{
+    	    System.out.println("Entered string is palindrome");
+    	
+    	}
+    else
+    	
+    	System.out.println("Entered string is not palindrome");
+    	
+    
+	}
+
+}
