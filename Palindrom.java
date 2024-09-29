403e0a8b388ea2a04cf9826a4ab81e2eb17f5fc0
Nisha00203 <138268793+Nisha00203@users.noreply.github.com>
2024-09-30 00:15:19 +0530

    Update Palindrom.java

--- Palindrom.java
+++ Palindrom.java
@@ -8,7 +8,7 @@ public static void main(String[] args)
 	
 	{
 		
-		String str = "madam";
+		String str = "Malayalam";
 		String output = "";
 		int len = str.length();
 		for (int i=len -1; i>=0 ;i--)
